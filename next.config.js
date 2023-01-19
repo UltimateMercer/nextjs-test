95ccdb9f708c192d11b78f4fa6d369599705de15
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2023-01-19 00:56:35 -0300

    Add assetsPrefix

--- next.config.js
+++ next.config.js
@@ -63,6 +63,7 @@ const securityHeaders = [
 
 module.exports = withBundleAnalyzer({
   basePath: "/nextjs-test",
+  assetPrefix: "/nextjs-test/",
   reactStrictMode: true,
   images: {
     domains: ["i.imgur.com", "miro.medium.com"],
